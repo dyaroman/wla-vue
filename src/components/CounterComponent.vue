c1239dccba58fad763b6104a30862fd7d3c41d31
Roman Diachenko <romand@leadsmarket.com>
2025-06-23 15:15:31 +0300

    update page title

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -1,7 +1,20 @@
 <script setup>
+import { watchEffect } from 'vue'
+
 import { useWebsitesStore } from '@/stores/websites.store'
+import { useMainStore } from '@/stores/main.store.js'
 
 const websitesStore = useWebsitesStore()
+const mainStore = useMainStore()
+
+watchEffect(() => {
+  let title = ''
+  if (websitesStore.visibleItems.length > 0) title += `[${websitesStore.visibleItems.length}]`
+  if (mainStore.env) title += `[${mainStore.env}]`
+  if (title.length > 0) title += ': '
+  title += 'WLA'
+  document.title = title
+})
 </script>
 
 <template>
