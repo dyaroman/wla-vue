fcfc9d49bfa7039c7ed8cf0e8d0cd06327a1bf32
Roman Diachenko <dyaroman@gmail.com>
2026-05-30 12:56:07 +0300

    Link footer to GitHub profile

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,3 +1,8 @@
 <template>
-  <footer class="footer">by <a href="slack://user?team=T1C5HJVG8&id=U9KK0QP2L">romand</a></footer>
+  <footer class="footer">
+    by
+    <a href="https://github.com/dyaroman" target="_blank" rel="noreferrer"
+      >dyaroman</a
+    >
+  </footer>
 </template>
